ae43682fe8b20bfc970c4efc358e60dc8ce780d7
Siddarth Pai <siddarthpaim@live.com>
2021-02-23 23:15:44 +0530

    Replace in proper folder

--- Water Table Maximisation/ContainerWithWaterProblem.py
+++ Water Table Maximisation/ContainerWithWaterProblem.py
@@ -1,20 +1,20 @@
 
-def maxArea(Area, Length) :
+def WaterTableMaximization(Area, Length) :
 	area = 0
 	for i in range(Length) :
 		for j in range(i + 1, Length) :
 		
-			# Calculating the max area
+		
 			area = max(area, min(Area[j], Area[i]) * (j - i))
 	return area
 
-# Driver code
-a = [ 1, 5, 4, 3 ]
-b = [ 3, 1, 2, 4, 5 ]
+
+a = [ 3, 8, 7, 6 ]
+b = [ 1, 5, 8, 3 ]
 
 len1 = len(a)
-print(maxArea(a, len1))
+print(WaterTableMaximization(a, len1))
 
 len2 = len(b)
-print(maxArea(b, len2))
+print(WaterTableMaximization(b, len2))
 
