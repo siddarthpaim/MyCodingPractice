0b094342b3d485bbd3922dcc2585f786e03b7e08
Siddarth Pai <siddarthpaim@live.com>
2021-03-11 19:51:22 +0530

    Maximum Area of Rectangle

---
+++ MaximumAreaOfRectangle/MaximumAreaOfRectangle.java
@@ -0,0 +1,16 @@
+public class MaximumAreaOfRectangle {
+
+    public int largestRectangleArea(int[] heights) {
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i; j < heights.length; j++) {
+                int minheight = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++)
+                    minheight = Math.min(minheight, heights[k]);
+                maxarea = Math.max(maxarea, minheight * (j - i + 1));
+            }
+        }
+        return maxarea;
+    }
+
+}
