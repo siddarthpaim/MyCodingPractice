7aec17da0495fbec15f56f3e10e84d1736998262
Siddarth Pai <siddarthpaim@live.com>
2021-03-11 19:54:58 +0530

    MaxArea

---
+++ MaximumAreaOfRectangle/MaxArea.java
@@ -0,0 +1,18 @@
+public class MaxArea {
+
+    public int calculateArea(int[] heights, int start, int end) {
+        if (start > end)
+            return 0;
+        int minindex = start;
+        for (int i = start; i <= end; i++)
+            if (heights[minindex] > heights[i])
+                minindex = i;
+        return Math.max(heights[minindex] * (end - start + 1),
+                Math.max(calculateArea(heights, start, minindex - 1), calculateArea(heights, minindex + 1, end)));
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        return calculateArea(heights, 0, heights.length - 1);
+    }
+
+}
