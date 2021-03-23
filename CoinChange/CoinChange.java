fa93ba0ac4d560ba6871852913db7f634c426799
Siddarth Pai <siddarthpaim@live.com>
2021-03-24 01:02:10 +0530

    Changes

--- CoinChange/CoinChange.java
+++ CoinChange/CoinChange.java
@@ -1,22 +1,25 @@
 public class CoinChange {
 
-    public int coinChange(int[] coins, int amount) {
-      if (amount < 1) return 0;
-      return coinChange(coins, amount, new int[amount]);
-    }
-  
-    private int coinChange(int[] coins, int rem, int[] count) {
-      if (rem < 0) return -1;
-      if (rem == 0) return 0;
-      if (count[rem - 1] != 0) return count[rem - 1];
-      int min = Integer.MAX_VALUE;
-      for (int coin : coins) {
-        int res = coinChange(coins, rem - coin, count);
-        if (res >= 0 && res < min)
-          min = 1 + res;
-      }
-      count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
-      return count[rem - 1];
-    }
+  public int coinChange(int[] coins, int amount) {
+    if (amount < 1)
+      return 0;
+    return coinChange(coins, amount, new int[amount]);
   }
-  #Not Missing Streak
+
+  private int coinChange(int[] coins, int rem, int[] count) {
+    if (rem < 0)
+      return -1;
+    if (rem == 0)
+      return 0;
+    if (count[rem - 1] != 0)
+      return count[rem - 1];
+    int min = Integer.MAX_VALUE;
+    for (int coin : coins) {
+      int res = coinChange(coins, rem - coin, count);
+      if (res >= 0 && res < min)
+        min = 1 + res;
+    }
+    count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+    return count[rem - 1];
+  }
+}
