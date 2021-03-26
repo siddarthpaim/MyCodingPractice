589eeaff640eb798faf988e44d0f9731c51a1f52
Siddarth Pai <siddarthpaim@live.com>
2021-03-27 01:43:01 +0530

    Shortest Word Dist

---
+++ ShortestWordDist/ShortestWordDist.java
@@ -0,0 +1,17 @@
+public class ShortestWordDist {
+
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                for (int j = 0; j < words.length; j++) {
+                    if (words[j].equals(word2)) {
+                        minDistance = Math.min(minDistance, Math.abs(i - j));
+                    }
+                }
+            }
+        }
+        return minDistance;
+    }
+
+}
