f49a62b18fec9993226dcb212c927953881c769e
Siddarth Pai <siddarthpaim@live.com>
2021-03-29 22:22:00 +0530

    Unique Path

---
+++ UniquePath/UniquePath.java
@@ -0,0 +1,10 @@
+public class UniquePath {
+
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+
+}
